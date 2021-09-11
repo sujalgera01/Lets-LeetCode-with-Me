95487a78aee063c1491f6af40d97f7f546017944
sujalgera01 <sujalgera01@gmail.com>
2021-09-11 23:44:50 +0530

    Create 1512. Number of Good Pairs.cpp

---
+++ Codes/1512. Number of Good Pairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int ct = 0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums.size();j++){
+                if(i<j && nums[i] == nums[j]){
+                    ct++;
+                }
+            }
+        }
+        return ct;
+    }
+};
