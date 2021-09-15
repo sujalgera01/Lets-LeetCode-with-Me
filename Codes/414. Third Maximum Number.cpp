3340774db6bdede9281b2240f4ee730a0978b6b3
sujalgera01 <sujalgera01@gmail.com>
2021-09-16 00:03:15 +0530

    Update 414. Third Maximum Number.cpp

--- Codes/414. Third Maximum Number.cpp
+++ Codes/414. Third Maximum Number.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        int n = nums.size();
-         sort(nums.begin(), nums.end());
-        nums.erase(unique(nums.begin(), nums.end()), nums.end());
-        if (n < 3) return nums[nums.size() - 1];
-        return nums[nums.size() - 3];
+        int ct = 1;
+        sort(nums.begin(),nums.end(),greater< int >());
+        if(nums.size()<3){
+            return nums[0];
+        }
+    
+        for(int i=1; i< nums.size(); i++){
+            if(nums[i]!=nums[i-1])
+                ct++;
+            if(ct==3)
+                return nums[i];
+        }
+    
+    return nums[0];
     }
 };
