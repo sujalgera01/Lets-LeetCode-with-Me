19d5792ab25063005be444e31d92a49b8a02320a
sujalgera01 <sujal.gera2019@vitstudent.ac.in>
2021-10-09 23:03:12 +0530

    Create 1732. Find the Highest Altitude.cpp

---
+++ Codes/1732. Find the Highest Altitude.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int n = gain.size();
+        int sum = 0;
+        vector<int> ans;
+        ans.push_back(0);
+        for(int i=0;i<n;i++){
+            sum += gain[i];
+            ans.push_back(sum);
+        }
+        return *max_element(ans.begin(),ans.end());
+    }
+};
