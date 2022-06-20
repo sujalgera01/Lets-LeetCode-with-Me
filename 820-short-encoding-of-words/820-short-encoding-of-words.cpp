2b2d3e49b1b4608c9553798a6eb2e0d83b8c40e4
sujalgera01 <sujal.gera2019@vitstudent.ac.in>
2022-06-20 16:32:09 +0530

    Time: 280 ms (9.91%), Space: 23.7 MB (55.45%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumLengthEncoding(vector<string>& words) {
+        set<string> s1(words.begin(),words.end());
+        set<string> s2(words.begin(),words.end());
+        
+        for(auto word: s1){
+            for(int i=1;i<word.size();i++){
+                s2.erase(word.substr(i));
+            }
+        }
+        
+        string s = "";
+        for(auto word : s2){
+            s += word + "#";
+        }
+        return s.size();
+    }
+};
